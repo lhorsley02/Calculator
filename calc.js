87431cafefc552775235b831350b9d4e2510cce8
Laura Horsley <horsleyshorses02@gmail.com>
2022-02-08 14:53:35 -0800

    All Four Working

--- calc.js
+++ calc.js
@@ -11,8 +11,26 @@ reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
 	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
+	num1 = 1;
+	num2 = 2;
+
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	} 
+	if (mathSymbol === "-"){
+		console.log(num1 - num2);
+	}
+	if (mathSymbol === "*"){
+		console.log(num1 * num2)
+	}
+	if (mathSymbol === "/"){
+		console.log(num1 / num2)
+	}
+
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
+
 
 
 	// This line closes the connection to the command line interface.
